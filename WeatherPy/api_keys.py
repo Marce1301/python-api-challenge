ffeebdc785b13194f07f64248c170df6e8ee5574
Marce1301 <marcelamarilu@gmail.com>
2023-02-28 22:13:31 -0600

    Marce

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = '45310f5ed7707d634e1fc04a617b885e'
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = '4229982b88f14a249dfb5ebe686cd778'
